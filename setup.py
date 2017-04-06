84461b8783758d3acee8fab9bbdf04090ea23832
wtjch <C-Wesley.Jinks@charter.com>
2017-04-05 18:24:40 -0600

    data files: parsers path

--- setup.py
+++ setup.py
@@ -1,5 +1,6 @@
 from setuptools import setup
 from setuptools import find_packages
+import setuptools
 from os import path
 
 here = path.abspath(path.dirname(__file__))
@@ -12,19 +13,20 @@ def readme():
 
 setup(
     name="mapscas",
-    version="0.1",
+    version="0.1.2",
 
+    include_package_data=True,
     packages=find_packages('src'),
     package_dir={'':'src'},
-    package_data={
-        '': ['*.dfa', '*.llr', '*.pyd']
-    },
+    #package_data={'': ['*.dfa', '*.llr', '*.pyd']},
     zip_safe=False,
+
+    data_files=[('../../Scripts', ['src/parsers/MapsClientParser.dfa', 'src/parsers/MapsClientParser.llr'])],
 
     # Metadata for PyPI
     author="Wesley Jinks",
     author_email="c-wesley.jinks@charter.com",
-    description="MAPS CAS API Package",
+    description='MAPS CAS API Package',
     url="https://github.com/wtjch/mapscas",
     license="Proprietary"
     )
